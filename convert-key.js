d43089735bc1d34bac5c0bc6a140eaf9ab59c10b
joey <jkokinda9@gmail.com>
2026-02-18 03:19:15 -0500

    clean repo

--- convert-key.js
+++
@@ -1,9 +0,0 @@
-const { PrivateKey } = require("@hashgraph/sdk");
-
-// Paste your Hedera private key here
-const hederaKey = "0x8fef27e316fb02c851c8e5cf82201d0cce54bf1459937844c732971e65caa62a";
-
-const pk = PrivateKey.fromStringDer(hederaKey);
-const ethereumKey = "0x" + pk.toStringRaw();
-
-console.log("Ethereum format:", ethereumKey);
